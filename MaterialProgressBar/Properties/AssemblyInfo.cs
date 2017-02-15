fd64882b73fb81b88360c128536ec47dd359a2c8
dminta <dominik@minta.it>
2017-02-16 00:47:00 +0100

    Fixed names

--- MaterialProgressBar/Properties/AssemblyInfo.cs
+++ MaterialProgressBar/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MaterialProgressBarXamarinAndroidBinding")]
+[assembly: AssemblyTitle("MaterialProgressBar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MaterialProgressBarXamarinAndroidBinding")]
+[assembly: AssemblyProduct("MaterialProgressBar")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
